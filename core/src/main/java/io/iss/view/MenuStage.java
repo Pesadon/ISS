42abb3186488ad8aafe0a39f35d05cf2d43dbd18
Stefano Calabrese <statix1@hotmail.it>
2024-11-27 12:50:19 +0100

    Translated strings to English

--- core/src/main/java/io/iss/view/MenuStage.java
+++ core/src/main/java/io/iss/view/MenuStage.java
@@ -8,13 +8,13 @@ import io.iss.controller.StageManager;
 
 public class MenuStage extends Stage {
     public MenuStage(final StageManager stageManager, Skin skin) {
-        TextButton playButton = new TextButton("Inizia Gioco", skin);
+        TextButton playButton = new TextButton("New game", skin);
         playButton.setPosition(300, 300);
 
         playButton.addListener(new ClickListener() {
             @Override
             public void clicked(com.badlogic.gdx.scenes.scene2d.InputEvent event, float x, float y) {
-                stageManager.switchStage("game"); // Cambia allo stage del gioco
+                stageManager.switchStage("game"); // Change to GameStage
             }
         });
 
