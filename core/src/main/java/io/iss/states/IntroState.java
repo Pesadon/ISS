45bf4eb906013b2748f59c3404564c21b75acaf9
LordMixy <diegoalongi4@gmail.com>
2024-12-02 15:46:57 +0100

    All hope is lost

--- core/src/main/java/io/iss/states/IntroState.java
+++ core/src/main/java/io/iss/states/IntroState.java
@@ -4,18 +4,23 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.ScreenUtils;
 import io.iss.factory.StateType;
 import io.iss.screens.GameScreen;
+import io.iss.utils.FontManager;
 import io.iss.utils.GameAssetManager;
 
 public class IntroState extends GameState {
     private final Image doorImage;
+    private final Label danteAdvice;
 
     public IntroState(GameScreen screen) {
         super(screen);
         doorImage = new Image(GameAssetManager.getInstance().get(GameAssetManager.DOOR_TEXTURE, Texture.class));
+        danteAdvice = new Label("All hope abandon ye who enter here", FontManager.getInstance().createRegularStyle());
     }
 
     @Override
@@ -36,13 +41,19 @@ public class IntroState extends GameState {
         doorImage.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                // When the door is clicked, we'll transition to the play state
-                screen.setState(screen.getStateFactory().createState(StateType.PLAY));
+            // When the door is clicked, we'll transition to the play state
+            screen.setState(screen.getStateFactory().createState(StateType.PLAY));
             }
         });
 
+        Table table = new Table();
+        table.setFillParent(true);
+        table.add(danteAdvice).padBottom(30).row();
+        table.add(doorImage).row();
+
         // Add the door to our stage
-        stage.addActor(doorImage);
+        // stage.addActor(doorImage);
+        stage.addActor(table);
 
         // Set up input processing
         Gdx.input.setInputProcessor(stage);
