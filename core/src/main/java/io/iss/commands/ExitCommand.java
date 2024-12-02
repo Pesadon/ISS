c9886f6833662ac60e191d2b3972fc568afc4b87
LordMixy <diegoalongi4@gmail.com>
2024-12-02 11:22:12 +0100

    EXITING

--- core/src/main/java/io/iss/commands/ExitCommand.java
+++ core/src/main/java/io/iss/commands/ExitCommand.java
@@ -1,8 +1,10 @@
 package io.iss.commands;
 
+import com.badlogic.gdx.Gdx;
+
 public class ExitCommand implements MenuCommand{
     @Override
     public void execute() {
-
+        Gdx.app.exit();
     }
 }
