1b5f2a931addd16ac0d19381b932690643dc450f
Stefano Calabrese <statix1@hotmail.it>
2024-12-18 11:26:52 +0100

    Add pause button menu to Dante's Door screen

--- core/src/main/java/io/iss/states/IntroState.java
+++ core/src/main/java/io/iss/states/IntroState.java
@@ -2,13 +2,18 @@ package io.iss.states;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import io.iss.dialogue.context.DialogueContext;
 import io.iss.factory.StateType;
 import io.iss.screens.GameScreen;
+import io.iss.ui.PauseMenu;
 import io.iss.utils.FontManager;
 import io.iss.utils.GameAssetManager;
 
@@ -17,8 +22,14 @@ public class IntroState extends GameState {
     private final Image danteImage;
     private final Label danteAdvice;
 
+    private final PauseMenu pauseMenu;
+    private final DialogueContext dialogueContext;
+
     public IntroState(GameScreen screen) {
         super(screen);
+
+        dialogueContext = new DialogueContext(screen, stage);
+        pauseMenu = new PauseMenu(screen, stage, dialogueContext);
 
         doorImage = new Image(GameAssetManager.getInstance().get(GameAssetManager.DOOR_TEXTURE, Texture.class));
         danteImage = new Image(GameAssetManager.getInstance().get(GameAssetManager.DANTE_TEXTURE, Texture.class));
@@ -65,6 +76,8 @@ public class IntroState extends GameState {
         stage.addActor(table);
         stage.addActor(danteImage);
 
+        initPauseButton();
+
         // Set up input processing
         Gdx.input.setInputProcessor(stage);
     }
@@ -74,5 +87,34 @@ public class IntroState extends GameState {
         stage.clear();
         stage.dispose();
         Gdx.input.setInputProcessor(null);
+    }
+
+    public void initPauseButton() {
+        // Load the image for the button (you can replace "pauseImage" with your image name)
+        ImageButton.ImageButtonStyle buttonStyle = new ImageButton.ImageButtonStyle();
+        buttonStyle.up = new TextureRegionDrawable(new TextureRegion(new Texture("assets/ui/pausebutton.png")));
+        buttonStyle.down = buttonStyle.up; // Optionally, add a "pressed" state image if needed
+
+        // Create the image button with the style
+        ImageButton pauseButton = new ImageButton(buttonStyle);
+
+        // Make the button square (adjust the size to your liking)
+        float buttonSize = 80f; // Set the size of the button
+        pauseButton.setSize(buttonSize, buttonSize);
+
+        // Position the button in the top-left corner
+        float buttonPadding = 20f; // Padding from the edges
+        pauseButton.setPosition(buttonPadding, Gdx.graphics.getHeight() - pauseButton.getHeight() - buttonPadding);
+
+        // Add the listener to handle the button click
+        pauseButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                pauseMenu.showPauseMenu(); // Show the pause menu when the button is clicked
+            }
+        });
+
+        // Add the button to the stage
+        stage.addActor(pauseButton);
     }
 }
